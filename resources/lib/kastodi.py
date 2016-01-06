feec205e68f069482a56cd6db5f2e312930abbe2
kharts <kharts@ukr.net>
2016-01-06 14:46:42 -0800

    Test dependencies import

---
+++ resources/lib/kastodi.py
@@ -0,0 +1,11 @@
+# kastodi.py - main module of the addon
+
+import sys
+from common import *
+
+import requests
+from google.protobuf import descriptor as _descriptor
+import six
+#from kodi.zeroconf import ServiceBrowser, Zeroconf
+
+debug(str(sys.path))
---
+++ service.py
@@ -0,0 +1,3 @@
+# service.py - starting point of the addon
+
+from resources.lib import kastodi
