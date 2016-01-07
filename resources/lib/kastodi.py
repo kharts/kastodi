1e63d63b3e83bcc41f3309e7b917033b56a7edb6
kharts <kharts@ukr.net>
2016-01-06 17:11:00 -0800

    Add copyright notice

--- resources/lib/kastodi.py
+++ resources/lib/kastodi.py
@@ -1,5 +1,9 @@
 # kastodi.py - main module of the addon
-
+#
+# Copyright 2016 kharts (https://github.com/kharts)
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
 
 import xbmc
 import xbmcgui
