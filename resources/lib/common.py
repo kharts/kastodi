5f04c062f4fe0d60f1078006c59961f185e33042
kharts <kharts@ukr.net>
2016-01-07 11:30:30 -0800

    Add copyright notice

--- resources/lib/common.py
+++ resources/lib/common.py
@@ -1,4 +1,9 @@
 # common.py - common functions
+#
+# Copyright 2016 kharts (https://github.com/kharts)
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
 
 import xbmc
 import xbmcaddon
