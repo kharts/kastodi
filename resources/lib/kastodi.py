cf23509a70a518eb566281fb14a2337a131413ec
kharts <kharts@ukr.net>
2016-01-06 17:10:28 -0800

    Try to place test button on the new window

--- resources/lib/kastodi.py
+++ resources/lib/kastodi.py
@@ -35,14 +35,17 @@ class CustomPlayer(xbmc.Player):
         :return: None
         """
 
+        info("onPlayBackStarted")
         test_button = xbmcgui.ControlButton(x=50,
                                             y=50,
                                             width=100,
                                             height=100,
                                             label="Cast")
-        player_window = xbmcgui.Window(PLAYER_CONTROLS_ID)
+        #player_window = xbmcgui.Window(PLAYER_CONTROLS_ID)
+        player_window = xbmcgui.Window()
         player_window.addControl(test_button)
         player_window.show()
+        test_button.setVisible(True)
 
 
 if __name__ == "__main__":
