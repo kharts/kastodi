8ba827ce1439e34e128c3bbe53a65808ec01cc41
kharts <kharts@ukr.net>
2016-01-19 20:46:47 -0800

    Add Cast button to the current skin

--- resources/lib/kastodi.py
+++ resources/lib/kastodi.py
@@ -10,8 +10,11 @@ import sys
 import os
 import mimetypes
 import urllib
+import shutil
+from xml.etree import ElementTree as ET
 import xbmc
 import xbmcgui
+import xbmcaddon
 from common import *
 
 
@@ -22,7 +25,6 @@ lib_path = os.path.join(addon_path, "resources", "lib")
 sys.path.insert(0, lib_path)
 debug(sys.path)
 import pychromecast
-debug("pychromecast is imported successfully")
 
 IDLE_TIME = 1 # 1 second
 PLAYER_CONTROLS_ID = 10114 # player controls window ID
@@ -35,17 +37,123 @@ def run():
     :return: None
     """
 
-    player = CustomPlayer()
+    #player = CustomPlayer()
+
+    result = add_cast_button()
 
     monitor = xbmc.Monitor()
 
     while not monitor.abortRequested():
-        player.check_cast_button_visibility()
+        #player.check_cast_button_visibility()
         #thread = threading.Thread(target=player.check_cast_button_visibility)
         #thread.start()
         if monitor.waitForAbort(IDLE_TIME):
             # Abort was requested while waiting. We should exit
             break
+
+
+def add_cast_button():
+    """
+    Add cast button to Video OSD Window of current skin
+    :return: True - if button was added successfully, False - otherwise
+    :rtype: bool
+    """
+
+    current_skin_id = xbmc.getSkinDir()
+    skin_folder_home = xbmc.translatePath("special://home/addons/" + current_skin_id)
+    if not os.path.exists(skin_folder_home):
+        #skin_folder_system = xbmc.translatePath("special://xbmc/addons/" + current_skin)
+        current_skin = xbmcaddon.Addon(current_skin_id)
+        skin_folder_system = xbmc.translatePath(current_skin.getAddonInfo("path"))
+        if not os.path.exists(skin_folder_system):
+            error("Couldn't find folder of skin " + current_skin_id)
+            return False
+        else:
+            try:
+                shutil.copytree(skin_folder_system, skin_folder_home)
+            except Exception, e:
+                log_exception(str(e))
+                message = "Couldn't copy skin from {0} to {1}".format(skin_folder_system,
+                                                                      skin_folder_home)
+                error(message)
+                return False
+    if not copy_icons(skin_folder_home):
+        return False
+    res_folder = get_default_resolution_folder(skin_folder_home)
+    if not res_folder:
+        error("Couldn't get default resolution folder for skin " + current_skin_id)
+        return False
+    videoosdxml_path = os.path.join(skin_folder_home, res_folder, "VideoOSD.xml")
+    with open(videoosdxml_path, "rb") as videoosdxml:
+        old_text = videoosdxml.read()
+    if old_text.find("<!-- kastodi_start -->") >= 0:
+        return True
+    cast_button_text = get_cast_button_text()
+    new_text = old_text.replace("</controls>", cast_button_text + "</controls>")
+    try:
+        with open(videoosdxml_path, "wb") as videoosdxml:
+            videoosdxml.write(new_text)
+    except Exception, e:
+        error("Couldn't write to skin folder")
+        log_exception(str(e))
+        return False
+    xbmc.executebuiltin("ReloadSkin()")
+    return True
+
+
+def copy_icons(skin_folder):
+    """
+    Copy icons from kastodi folder to the folder of current skin
+    :param skin_folder: path to current skin folder
+    :type skin_folder: str
+    :return: True - if icons were successfully copied, False - otherwise
+    :rtype: bool
+    """
+
+    for icon in ["ic_cast_blue_24dp.png", "ic_cast_white_24dp.png"]:
+        try:
+            src = image(icon)
+            dst = os.path.join(skin_folder, "media", icon)
+            shutil.copyfile(src, dst)
+        except Exception, e:
+            log_exception(str(e))
+            message = "Couldn't copy icon from {0} to {1}".format(src, dst)
+            error(message)
+            return False
+    return True
+
+
+def get_default_resolution_folder(skin_folder):
+    """
+    Get default resolution folder for current skin
+    :return: short name of the folder, example "720p"
+    :rtype: str
+    """
+
+    addonxmlname = os.path.join(skin_folder, "addon.xml")
+    try:
+        tree = ET.parse(addonxmlname)
+    except Exception, e:
+        log_exception("Couldn't parse " + addonxmlname)
+        log_exception(str(e))
+        return None
+    root = tree.getroot()
+    for child in root:
+        if child.tag == "extension":
+            if child.attrib.get("point", "") == "xbmc.gui.skin":
+                for child2 in child:
+                    if child2.tag == "res":
+                        if child2.attrib.get("default", "") == "true":
+                            return child2.attrib.get("folder", None)
+    return None
+
+
+def get_cast_button_text():
+    path = os.path.join(addon_path, "resources", "skin", "CastButton.txt")
+    text = ""
+    with open(path, "r") as cast_button_template:
+        text = cast_button_template.read()
+    return text
 
 
 class CustomPlayer(xbmc.Player):
